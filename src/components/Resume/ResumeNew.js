ffdc940b1dfa1ee3d56a4d9190bd71ec41a81617
world970511 <57740138+world970511@users.noreply.github.com>
2022-03-31 22:57:26 +0900

    fix:pdf 호출 에러 수정

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/world970511/world970511.github.io/blob/main/src/Assets/showProfile.pdf";
+  "https://raw.githubusercontent.com/world970511/world970511.github.io/main/src/Assets/ParkNaeun_profile.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
