fa24afd51365079f4f3c9e43f2b76ea61ccf72d2
world970511 <world970511@naver.com>
2025-07-25 08:50:55 +0900

    블로그 favicon 설정

--- assets/js/build-blog.js
+++ assets/js/build-blog.js
@@ -114,6 +114,13 @@ class BlogBuilder {
     <meta property="og:title" content="${post.title}">
     <meta property="og:description" content="${post.excerpt}">
     
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="../images/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="../images/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="../images/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="../images/favicon/apple-touch-icon.png" />
+    <link rel="manifest" href="../images/favicon/site.webmanifest" />
+    
     <link rel="stylesheet" href="../../assets/css/styles.css">
     <link rel="stylesheet" href="../../assets/css/blog-post.css">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap" rel="stylesheet">
